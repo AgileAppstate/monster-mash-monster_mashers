bfc399574d457e74b37b6bc71110ce9c6929b59d
wrightcn2 <wrightcn1@appstate.edu>
2025-10-27 13:29:15 -0400

    Added Verse 1 and Chorus 1

---
+++ MonsterMash.java
@@ -0,0 +1,19 @@
+
+public class MonsterMash {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Monster Mash!");
+
+        System.out.println("Lyrics:");
+
+        System.out.println("Verse 1: I was working in the lab, late one night"
+                             + "When my eyes beheld an eerie sight"
+                             + "For my monster, from his slab, began to rise"
+                             + "And suddenly to my surprise");
+
+        System.out.println("Chorus 1: (He did the mash) he did the monster mash"
+                            + "(The monster mash) it was a graveyard smash"
+                            + "(He did the mash) it caught on in a flash"
+                            + "(He did the mash) he did the monster mash");
+    }
+}
